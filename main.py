2b99f551d668f33590a42c8da2430f3e8b382138
lia_161 <lia.angerer@systemli.org>
2021-03-08 03:03:38 +0100

    Bugfix + new function (delete) + deleted 'Wer im Raum'

--- main.py
+++ main.py
@@ -39,19 +39,16 @@ while True:
                      "mit /wahrheit wird eine zufällige Wahrheits-Frage ausgewählt. Wenn du einen Username angibst geht die Frage direkt an die Person.\n"
                      "mit /pflicht wird eine zufällige Pflicht-Aufgabe ausgewählt (Hinweis: Die meisten sind nicht während Corona machbar).Wenn du einen Username angibst geht die Frage direkt an die Person.\n"
                      "mit /i_have_never wird eine zufällige 'Ich hab noch nie'-Frage ausgewählt.\n"
-                     "mit /who wird eine zufällige 'Wer im Raum...?'-Frage ausgewählt.\n"
                      "mit /who_would_rather wird eine zufällige 'Wer würde eher...?'-Frage ausgewählt.\n"
                      "mit /next wird eine zufällige Kategorie der oben aufgezählten Kategorien genannt.\n\n"
                      "Wenn ihr mehr Fragen habt, könnt ihr die mit folgenden Befehlen hinzufügen:\n\n"
                      "/add_truth - Wahrheit hinzufügen\n"
                      "/add_dutie - Pflicht hinzufügen\n"
                      "/add_never - 'Ich hab noch nie' hinzufügen\n"
-                     "/add_room - 'Wer im Raum' hinzufügen\n"
                      "/add_rather - 'Wer würde eher' hinzufügen\n"
                      "Viel Spaß!\n\n"
-                     f"Es gibt insgesamt:\n{str(len(data['Tasks']['Truth']))} Wahrheitsfragen,\n{len(data['Tasks']['Dare'])} Pflicht-Aufgaben, \n{len(data['Tasks']['Never'])} Ich hab noch nie Fragen, \n{len(data['Tasks']['Rather'])} Wer würde eher Fragen, \n{len(data['Tasks']['Room'])} Wer im Raum Fragen.\n"
-                     f"Insgesamt also {len(data['Tasks']['Truth']) + len(data['Tasks']['Dare']) + len(data['Tasks']['Never']) + len(data['Tasks']['Rather']) + len(data['Tasks']['Room'])} Fragen.")
-
+                     f"Es gibt insgesamt:\n{str(len(data['Tasks']['Truth']))} Wahrheitsfragen,\n{len(data['Tasks']['Dare'])} Pflicht-Aufgaben, \n{len(data['Tasks']['Never'])} Ich hab noch nie Fragen und \n{len(data['Tasks']['Rather'])} Wer würde eher Fragen.\n"
+                     f"Insgesamt also {len(data['Tasks']['Truth']) + len(data['Tasks']['Dare']) + len(data['Tasks']['Never']) + len(data['Tasks']['Rather'])} Fragen.")
 
     # Wahrheit
     @bot.message_handler(commands=['wahrheit'])
@@ -61,7 +58,6 @@ while True:
         truth = w[rand]
         bot.reply_to(message, truth)
 
-
     # Pflicht
     @bot.message_handler(commands=['pflicht'])
     def send_dutie(message):
@@ -69,7 +65,6 @@ while True:
         rand = randint(0, len(w) - 1)
         dutie = w[rand]
         bot.reply_to(message, dutie)
-
 
     # Ich habe noch nie
     @bot.message_handler(commands=['i_have_never'])
@@ -79,16 +74,6 @@ while True:
         never = w[rand]
         bot.reply_to(message, never)
 
-
-    # Wer im Raum?
-    @bot.message_handler(commands=['who'])
-    def send_who_in_the_room(message):
-        w = data["Tasks"]["Room"]
-        rand = randint(0, len(w) - 1)
-        room = w[rand]
-        bot.reply_to(message, room)
-
-
     # Wer würde eher...?
     @bot.message_handler(commands=['who_would_rather'])
     def who_would_rather(message):
@@ -97,11 +82,9 @@ while True:
         rather = w[rand]
         bot.reply_to(message, rather)
 
-
     @bot.message_handler(commands=['wop', 'WoP'])
     def next_one(message):
         bot.reply_to(message, data["Categories"][randint(0, len(data["Categories"]) - 1)])
-
 
     # Add Questions and Duties
     @bot.message_handler(commands=['add_truth'])
@@ -131,7 +114,6 @@ while True:
                 print(e)
         json_write(data)
 
-
     @bot.message_handler(commands=['add_dutie', 'add_dare'])
     def add_dutie(message):
         try:
@@ -159,35 +141,6 @@ while True:
                 print(e)
         json_write(data)
 
-
-    @bot.message_handler(commands=['add_room'])
-    def add_room(message):
-        try:
-            text = message.reply_to_message.text
-            for i in data["Tasks"]:
-                if text not in i["Room"]:
-                    i["Room"].append(text)
-                    bot.reply_to(message, "Erfolgreich hinzugefügt ✅")
-                else:
-                    bot.reply_to(message, "Text schon hinzugefügt!")
-        except AttributeError:
-            try:
-                command = len(message.text.split()[0]) + 1
-                text = message.text[command:]
-                for i in data["Tasks"]:
-                    if text not in i["Room"]:
-                        i["Room"].append(text)
-                        bot.reply_to(message, "Erfolgreich hinzugefügt ✅")
-                    else:
-                        bot.reply_to(message, "Text schon hinzugefügt!")
-            except ValueError:
-                bot.reply_to(message, "Du musst einen Text angeben oder auf eine Nachricht antworten!")
-            except Exception as e:
-                bot.reply_to(message, "Irgendwas ist schiefgelaufen.")
-                print(e)
-        json_write(data)
-
-
     @bot.message_handler(commands=['add_rather'])
     def add_rather(message):
         try:
@@ -214,7 +167,6 @@ while True:
                 bot.reply_to(message, "Irgendwas ist schiefgelaufen.")
                 print(e)
         json_write(data)
-
 
     @bot.message_handler(commands=['add_never'])
     def add_never(message):
@@ -245,6 +197,135 @@ while True:
                 print(e)
         json_write(data)
 
+# Löschen
+    @bot.message_handler(commands=['delete_truth'])
+    def del_truth(message):
+        try:
+            text = message.reply_to_message.text
+            for i in data["Tasks"]:
+                if text in i["Truth"]:
+                    i["Truth"].remove(text)
+            bot.reply_to(message, "Text aus Wahrheiten entfernt!")
+        except AttributeError:
+            try:
+                command = len(message.text.split()[0])+1
+                text = message.text[command:]
+                for i in data["Tasks"]:
+                    if text in i["Truth"]:
+                        i["Truth"].remove(text)
+                bot.reply_to(message, "Text aus Wahrheiten entfernt!")
+            except ValueError:
+                bot.reply_to(message, "Du musst einen Text angeben oder auf eine Nachricht antworten!")
+
+    @bot.message_handler(commands=['delete_dare'])
+    def del_dare(message):
+        try:
+            text = message.reply_to_message.text
+            for i in data["Tasks"]:
+                if text in i["Dare"]:
+                    i["Dare"].remove(text)
+            bot.reply_to(message, "Text aus Pflichten entfernt!")
+        except AttributeError:
+            try:
+                command = len(message.text.split()[0]) + 1
+                text = message.text[command:]
+                for i in data["Tasks"]:
+                    if text in i["Dare"]:
+                        i["Dare"].remove(text)
+                bot.reply_to(message, "Text aus Pflichten entfernt!")
+            except ValueError:
+                bot.reply_to(message, "Du musst einen Text angeben oder auf eine Nachricht antworten!")
+
+    @bot.message_handler(commands=['delete_rather'])
+    def del_rather(message):
+        try:
+            text = message.reply_to_message.text
+            for i in data["Tasks"]:
+                if text in i["Rather"]:
+                    i["Rather"].remove(text)
+            bot.reply_to(message, "Text aus 'Wer würde eher' entfernt!")
+        except AttributeError:
+            try:
+                command = len(message.text.split()[0]) + 1
+                text = message.text[command:]
+                for i in data["Tasks"]:
+                    if text in i["Rather"]:
+                        i["Rather"].remove(text)
+                bot.reply_to(message, "Text aus 'Wer würde eher' entfernt!")
+            except ValueError:
+                bot.reply_to(message, "Du musst einen Text angeben oder auf eine Nachricht antworten!")
+
+    @bot.message_handler(commands=['delete_never'])
+    def del_never(message):
+        try:
+            text = message.reply_to_message.text
+            for i in data["Tasks"]:
+                if text in i["Never"]:
+                    i["Never"].remove(text)
+            bot.reply_to(message, "Text aus 'Ich hab noch nie' entfernt!")
+        except AttributeError:
+            try:
+                command = len(message.text.split()[0]) + 1
+                text = message.text[command:]
+                for i in data["Tasks"]:
+                    if text in i["Never"]:
+                        i["Never"].remove(text)
+                bot.reply_to(message, "Text aus 'Ich hab noch nie' entfernt!")
+            except ValueError:
+                bot.reply_to(message, "Du musst einen Text angeben oder auf eine Nachricht antworten!")
+
+    @bot.message_handler(commands=['delete'])
+    def delete(message):
+        try:
+            text = message.reply_to_message.text
+            for i in data["Tasks"]:
+                yes = True
+                if text in i["Truth"]:
+                    i["Truth"].remove(text)
+                    yes = False
+                if text in i["Dare"]:
+                    i["Dare"].remove(text)
+                    yes = False
+                if text in i["Room"]:
+                    i["Room"].remove(text)
+                    yes = False
+                if text in i["Rather"]:
+                    i["Rather"].remove(text)
+                    yes = False
+                if text in i["Never"]:
+                    i["Never"].remove(text)
+                    yes = False
+                if yes:
+                    bot.reply_to(message, "Text nicht Gespeichert. Mit '/add_<FUNCTION>' hinzufügen!")
+                else:
+                    bot.reply_to(message, "Erfolgreich entfernt!")
+        except AttributeError:
+            try:
+                command = len(message.text.split()[0])+1
+                text = message.text[command:]
+                for i in data["Tasks"]:
+                    yes = True
+                    if text in i["Truth"]:
+                        i["Truth"].remove(text)
+                        yes = False
+                    if text in i["Dare"]:
+                        i["Dare"].remove(text)
+                        yes = False
+                    if text in i["Room"]:
+                        i["Room"].remove(text)
+                        yes = False
+                    if text in i["Rather"]:
+                        i["Rather"].remove(text)
+                        yes = False
+                    if text in i["Never"]:
+                        i["Never"].remove(text)
+                        yes = False
+                    if yes:
+                        bot.reply_to(message, "Text nicht Gespeichert. Mit '/add_<FUNCTION>' hinzufügen!")
+                    else:
+                        bot.reply_to(message, "Erfolgreich entfernt!")
+            except ValueError:
+                bot.reply_to(message, "Du musst einen Text angeben oder auf eine Nachricht antworten!")
 
     try:
         print("Start Polling     " + str(datetime.now()))
